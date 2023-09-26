51a73c7e76f60f945bd15cfac2b66aec7def8168
AlanAvila97 <fidelavila21@gmail.com>
2023-09-26 16:53:08 -0600

    configuracion repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,22 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/image',
+    'nuxt-gtag',
+    'nuxt-purgecss',
+    'nuxt-link-checker',
+    'nuxt-delay-hydration',
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  purgecss: {
+    enabled: false, // Always enable purgecss
+    // safelist: ['my-class'], // Add my-class token to the safelist (e.g. .my-class)
+  },
+  delayHydration: {
+    // enables nuxt-delay-hydration in dev mode for testing
+    debug: process.env.NODE_ENV === 'development'
+  },
+  gtag: {
+    // id: 'G-XXXXXXXXXX',
+    // initialConsent: false
+  }
 })
