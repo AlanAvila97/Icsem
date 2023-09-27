ac94732be5a5ce7451d37a92bdea7b643876acab
AlanAvila97 <fidelavila21@gmail.com>
2023-09-26 23:16:29 -0600

    Componentes globales de navegacion

--- components/NavbarView.vue
+++ components/NavbarView.vue
@@ -1,7 +1,35 @@
 <template>
-    <div>
+    <header class="header fixed-top header-once">
+        <div class="container-header">
 
-    </div>
+            <div class="content-menu-hamburguer">
+            </div><!-- End div content menu hamburguer -->
+            <div class="logo">
+                <nuxt-link to="/">                    
+                    Home
+                </nuxt-link>
+            </div><!-- End div logo -->
+            <div class="sections-nav">
+                <ul class="elements-nav">
+                    <li>   
+                        <nuxt-link to="/Delay">
+                            Delay
+                        </nuxt-link>
+                    </li>
+                    <li>   
+                        <nuxt-link to="/Contactanos">
+                            Contactanos
+                        </nuxt-link>
+                    </li>
+                    <li>   
+                        <nuxt-link to="/Contactanos">
+                            Contactanos
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </div><!-- End div sections nav -->
+        </div>
+    </header>
 </template>
 
 <script setup>
---
+++ components/RedesSociales.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="content-social-networks">
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
