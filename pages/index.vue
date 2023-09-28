4079731fb3f4e7b4fb0d712ee0c45f8b3c665f1f
AlanAvila97 <fidelavila21@gmail.com>
2023-09-27 19:20:58 -0600

    diseño de slider principal

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,92 @@
 <template>
     <main id="wrapper" class=" index">
         <section class="pt-md-70 section-slider-head">
-            <h1>Home</h1>
+           <div class="container-slider-head">
+                <swiper 
+                    :direction="'vertical'"
+                    :slidesPerView="1"
+                    :spaceBetween="30"
+                    :rewind="true"
+                    :mousewheel="true"
+                    :grabCursor="true"                  
+
+                    :pagination='{
+                        clickable: true,
+                        dynamicBullets: true,
+                        dynamicMainBullets: 1,
+                        renderBullet: function (index, className) {
+                            let title = "paginacion-"+(index + 1);
+                            return "<span id="+title+" class="+className+" title="+title+"></span>";
+                        }
+                    }'                    
+                    :modules="modules" 
+                    class="swiper-head">
+                    <swiper-slide>
+                        <NuxtPicture
+                            src="images/carousel-1.jpg"
+                            densities="x1 x2"
+                            preset="blog" 
+                            format="webp" 
+                            quality="90" 
+                            fit="cover" 
+                            preload
+                            :imgAttrs="{
+                                id:'my-id', 
+                                class:'my-class', 
+                                style:'display:block', 
+                                alt: 'Image',                                
+                                title: 'Imagen',
+                                'data-my-data': 'my-value'}"/>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <NuxtPicture
+                            src="images/carousel-2.jpg"
+                            densities="x1 x2"
+                            preset="blog" 
+                            format="webp" 
+                            quality="90" 
+                            fit="cover" 
+                            preload
+                            :imgAttrs="{
+                                id:'my-id', 
+                                class:'my-class', 
+                                style:'display:block', 
+                                alt: 'Image',                                
+                                title: 'Imagen',
+                                'data-my-data': 'my-value'}"/>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <NuxtPicture
+                            src="images/carousel-3.jpg"
+                            densities="x1 x2"
+                            preset="blog" 
+                            format="webp" 
+                            quality="90" 
+                            fit="cover" 
+                            preload
+                            :imgAttrs="{
+                                id:'my-id', 
+                                class:'my-class', 
+                                style:'display:block', 
+                                alt: 'Image',                                
+                                title: 'Imagen',
+                                'data-my-data': 'my-value'}"/>
+                    </swiper-slide>
+                </swiper>
+           </div>
         </section>
     </main>
 </template>
 
 <script setup>
+    // Vue
+    import { onMounted } from "vue";
+    // Import Swiper Vue.js components
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+    import { Pagination, Navigation, Mousewheel, Autoplay } from 'swiper/modules';
+    // 
+    const modules =  [Navigation, Pagination, Mousewheel, Autoplay];
+
 </script>
 
 <style scoped>
