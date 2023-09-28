6a96eb57873f5dfcc30fec5672d50db4cb1b7354
AlanAvila97 <fidelavila21@gmail.com>
2023-09-27 19:20:20 -0600

    Diseño de navbar y seccion de redes sociales

--- components/NavbarView.vue
+++ components/NavbarView.vue
@@ -1,29 +1,48 @@
 <template>
-    <header class="header fixed-top header-once">
+    <!-- <RedexsSociales></RedexsSociales> -->
+    <header class="header fixed-header-top header-once">
         <div class="container-header">
-
             <div class="content-menu-hamburguer">
+                <label for="check" class="menuButton">
+                    <input id="check" type="checkbox">
+                    <span class="top"></span>
+                    <span class="mid"></span>
+                    <span class="bot"></span>
+                </label>    
             </div><!-- End div content menu hamburguer -->
             <div class="logo">
                 <nuxt-link to="/">                    
-                    Home
+                    <NuxtPicture
+                        src="images/logo_icsem.png"
+                        densities="x1 x2"
+                        preset="blog" 
+                        format="webp" 
+                        quality="90" 
+                        fit="cover" 
+                        preload
+                        :imgAttrs="{
+                            id:'Logo', 
+                            class:'', 
+                            style:'', 
+                            alt: 'Logo Icsem',                                
+                            title: 'Logo Icsem'}"/>
                 </nuxt-link>
             </div><!-- End div logo -->
             <div class="sections-nav">
                 <ul class="elements-nav">
                     <li>   
-                        <nuxt-link to="/Delay">
+                        <nuxt-link to="/delay">
                             Delay
                         </nuxt-link>
                     </li>
                     <li>   
-                        <nuxt-link to="/Contactanos">
+                        <nuxt-link to="/contactanos">
                             Contactanos
                         </nuxt-link>
                     </li>
                     <li>   
-                        <nuxt-link to="/Contactanos">
-                            Contactanos
+                        <nuxt-link to="/productos/1">
+                            Productos
                         </nuxt-link>
                     </li>
                 </ul>
--- components/RedesSociales.vue
+++ components/RedesSociales.vue
@@ -1,6 +1,20 @@
 <template>
-    <div class="content-social-networks">
-        
+    <div class="content-social-networks d-flex">
+        <div class="info">
+            <Icon name="subway:location"/>
+            <h2>Ubicacion</h2>
+        </div>
+        <div class="social-networks d-flex">
+            <a href="/" target="_blank" rel="noopener noreferrer" aria-label="">
+                <Icon name="fa-brands:facebook-f"/>
+            </a>
+            <a href="/" target="_blank" rel="noopener noreferrer" aria-label="">
+                <Icon name="fa-brands:twitter"/>
+            </a>
+            <a href="/" target="_blank" rel="noopener noreferrer" aria-label="">
+                <Icon name="fa-brands:instagram"/>                
+            </a>
+        </div>
     </div>
 </template>
 
