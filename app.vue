e8840c363d2274506323891e912bc6483f108fc5
AlanAvila97 <fidelavila21@gmail.com>
2023-10-03 18:23:50 -0600

    css globales, link current active, json de estados,

--- app.vue
+++ app.vue
@@ -2,28 +2,34 @@
     <NuxtLayout>
         <NuxtPage />
     </NuxtLayout>
-  </template>
-  
-  <style>
-
-
-    .page-enter-active,
-    .page-leave-active {
-      transition: all 0.4s;
-    }
-    .page-enter-from,
-    .page-leave-to {
-      opacity: 0;
-      filter: blur(1rem);
-    }
-    .layout-enter-active,
-    .layout-leave-active {
-      transition: all 0.4s;
-    }
-    .layout-enter-from,
-    .layout-leave-to {
-      opacity: 0;
-      filter: blur(1rem);
-    }
+</template>
+<style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.4s;
+  }
+  .page-enter-from,
+  .page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+  }
+  .layout-enter-active,
+  .layout-leave-active {
+    transition: all 0.4s;
+  }
+  .layout-enter-from,
+  .layout-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+  }
+  .router-link-active {
     
-  </style>
+  }
+  .router-link-active:hover {
+      color: var(--green-logo-color)  ;    
+  }
+  .router-link-exact-active{
+    color: var(--green-logo-color)!important;
+    font-weight: bold;
+  }
+</style>
