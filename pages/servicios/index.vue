5197383e5b4cc630c015293b80d3641de96a0ecb
AlanAvila97 <fidelavila21@gmail.com>
2023-10-04 00:28:03 -0600

    creacion de vistas sin parametros

--- pages/servicios/index.vue
+++ pages/servicios/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <main id="wrapper" class="product">
-        <section class="pt-md-70 section-products">
-        <h2 style="color:#000;">{{ $route.params.id }}</h2>
+    <main id="wrapper" class="experience">
+        <section class="pt-md-70 section-contact">
+        <h2 style="color:#000;">Servicios</h2>
         </section>
     </main>
 </template>
@@ -14,6 +14,6 @@
     const router = useRouter();
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
